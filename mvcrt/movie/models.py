9b00b2f686f22a746a1ae8b44c2d4d3fbe3c2c88
Ohjin <qpqpq13579@gmail.com>
2020-03-25 05:58:15 +0900

    Add movie model

--- mvcrt/movie/models.py
+++ mvcrt/movie/models.py
@@ -3,4 +3,7 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=200)
-    
+    starring = models.CharField(max_length=200)
+    summary = models.CharField(max_length=2000)
+    genre = models.CharField(max_length=20)
+    pub_date = models.DateTimeField('date published')
