8fb20a1509a80b4a9f4258b29267c5e171fb6017
Ohjin <qpqpq13579@gmail.com>
2020-03-26 08:20:15 +0900

    Add home path

--- mvcrt/mvcrt/urls.py
+++ mvcrt/mvcrt/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from main import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.HomeView.as_view(), name='home'),
     path('movie/', include('movie.urls')),
+    path('main/', include('main.urls'))
 ]
