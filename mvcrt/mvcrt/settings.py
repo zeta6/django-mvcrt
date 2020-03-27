e45d7e98ef77bce91043ba95089ccf00bd4757c9
Ohjin <qpqpq13579@gmail.com>
2020-03-27 20:30:49 +0900

    Add movie.tampaltetags to setting

--- mvcrt/mvcrt/settings.py
+++ mvcrt/mvcrt/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
     'movie.apps.MovieConfig',
+    'movie.templatetags',
+    'account.apps.AccountConfig',
 ]
 
 MIDDLEWARE = [
