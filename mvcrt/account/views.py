075f88dcbd91ef5a9e301a9ccfe8c8124a03c7ee
Ohjin <qpqpq13579@gmail.com>
2020-04-02 17:28:01 +0900

    add login, signup, logout

--- mvcrt/account/views.py
+++ mvcrt/account/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views import generic
+from django.urls import reverse
+from django.shortcuts import get_object_or_404, render, redirect
+
+from .models import Account
+
+def sign_up(request):
+    if request.method == "POST":
+        account_name = request.POST.get('account_name')
+        password = request.POST.get('password')
+        Account(
+            account_name = account_name,
+            password = password
+        ).save()
+        return redirect(reverse('main:home'))
+
+    return render(request, 'account/sign_up.html')
+
+def login(request):
+    if request.method == "POST":
+        account_name = request.POST.get('account_name')
+        password = request.POST.get('password')
+        account = Account.objects.get(account_name=account_name)
+        if account.password == password:
+            request.session['account_name'] = account_name
+        return redirect(reverse('main:home'))
+
+    return render(request, 'account/login.html')
+
+def logout(request):
+    try:
+        del request.session['account_name']
+    except KeyError:
+        pass
+    return redirect(reverse('main:home'))
